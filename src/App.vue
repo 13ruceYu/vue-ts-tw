a7192c0e70ce871fe39c614e2b22c91e9f935438
13ruceYu <bruceyuhongbo@gmail.com>
2025-01-12 15:33:32 +0800

    feat: add iconify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div class="h-screen flex justify-center items-center">
-    vue + ts + tailwindcss
+  <div class="h-screen flex justify-center items-center gap-2">
+    <div class="flex gap-2 items-center">
+      <Icon icon="skill-icons:vuejs-light" class="size-8" />
+      <Icon class="size-8" icon="skill-icons:typescript" />
+      <Icon class="size-8" icon="skill-icons:tailwindcss-light" />
+      <span>Iconify</span>
+    </div>
   </div>
 </template>
